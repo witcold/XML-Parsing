5c37b66a560349a0b6ea2a8a8e22714d9da6188a
Vitold Meshcheryakov <Vitold.Meshcheryakov@dataart.com>
2014-08-27 09:54:51 +0000

    Changed build configuration

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-java -classpath target/practice-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.dataart.Main
+java -classpath target/practice-0.0.1-SNAPSHOT.jar com.dataart.Main
 pause
 
---
+++ run2.bat
@@ -0,0 +1,3 @@
+java -jar target/practice-0.0.1-SNAPSHOT.jar
+pause
+
